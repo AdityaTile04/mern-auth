3a5f1dee29b49627c54940806ccdfe4df682533a
Aditya Tile <adityatile99@gmail.com>
2024-01-21 17:15:14 +0530

    create test api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,7 @@
+export const test = (req, res) => {
+  (req, res) => {
+    res.json({
+      message: "API ia working",
+    });
+  };
+};
--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
+import userRoutes from "./routes/user.route.js";
 
 mongoose
   .connect(process.env.MONGO)
@@ -17,3 +18,5 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
+
+app.use("/api/user", userRoutes);
