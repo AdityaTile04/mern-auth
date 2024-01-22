5cc5d21bcbe6262f93e9a653a954ac34775e017c
Aditya Tile <adityatile99@gmail.com>
2024-01-22 10:43:18 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-dotenv.config();
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+dotenv.config();
 
 mongoose
   .connect(process.env.MONGO)
@@ -15,8 +16,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -20,6 +20,6 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const user = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
